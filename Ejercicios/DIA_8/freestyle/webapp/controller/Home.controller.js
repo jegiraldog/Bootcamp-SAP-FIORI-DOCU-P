0a010bd12c126058d463f6b07a5d18548382ceaf
johanyglobant <119626012+johanyglobant@users.noreply.github.com>
2025-03-14 12:47:23 -0500

    Update Ejercicio 8

---
+++ Ejercicios/DIA_8/freestyle/webapp/controller/Home.controller.js
@@ -0,0 +1,16 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "com/bootcamp/sapui5/freestyle/utils/HomeHelper"
+], (Controller, HomeHelper) => {
+    "use strict";
+
+    return Controller.extend("com.bootcamp.sapui5.freestyle.controller.Home", {
+        onInit() {
+        },
+
+        onPress: async function(){
+            let oDatos = await HomeHelper.getDataProducts(); 
+            await HomeHelper.setProductModel(this, oDatos[0].results);
+        }
+    });
+});
