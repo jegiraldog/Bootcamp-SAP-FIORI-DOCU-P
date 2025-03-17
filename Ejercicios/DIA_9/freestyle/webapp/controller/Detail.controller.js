359cd26fef464e7957fff70e4b184ee5360c117a
johanyglobant <119626012+johanyglobant@users.noreply.github.com>
2025-03-17 11:43:45 -0500

    Ejercicios Dia 9

---
+++ Ejercicios/DIA_9/freestyle/webapp/controller/Detail.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "com/bootcamp/sapui5/freestyle/utils/HomeHelper"
+], (Controller, HomeHelper) => {
+    "use strict";
+
+    return Controller.extend("com.bootcamp.sapui5.freestyle.controller.Detail", {
+        onInit() {
+        }
+    });
+});
